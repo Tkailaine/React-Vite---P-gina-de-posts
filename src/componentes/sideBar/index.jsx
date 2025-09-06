a5b13ddbf1b66da91c03c1ea1594493f23d9867c
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2025-09-06 18:51:30 -0300

    feat: componetizando links do SideBar

--- src/componentes/sideBar/index.jsx
+++ src/componentes/sideBar/index.jsx
@@ -1,9 +1,40 @@
 import Logo from './assets/Logo.png'
+import Feed from './assets/feed.svg'
+import Info from './assets/info.svg'
+import Perfil from './assets/perfil.svg'
+import Logout from './assets/logout.svg'
 
 export default function Sidebar(){
+
+        function LinkSideBar({icone, children}){
+
+            return(   
+            <li>
+                <a href="#"><img src={icone} alt="Ícone de Feed" /><span>{children}</span></a>
+            </li>
+                )
+            }
+            
+        
+        function Botao({children}){
+            return(
+                <button>{children}</button>
+            )
+        }
+
    return(
     <aside>
         <img src={Logo} alt="Logo do CodeConnect" />
+
+        <ul>
+           <LinkSideBar icone={Feed}>Feed</LinkSideBar>
+           
+            <LinkSideBar icone={Perfil}>Perfil</LinkSideBar>
+
+            <LinkSideBar icone={Info}>Sobre nós</LinkSideBar>
+
+            <LinkSideBar icone={Logout}>Logout</LinkSideBar>
+        </ul>
     </aside>
 
    )
