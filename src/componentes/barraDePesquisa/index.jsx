33a8e6dd4cb7943399a3a3cbc9104a46733fe027
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2025-09-06 20:40:48 -0300

    feat: adicionando useState na barra de pesquisa

--- src/componentes/barraDePesquisa/index.jsx
+++ src/componentes/barraDePesquisa/index.jsx
@@ -1,10 +1,12 @@
+import {useState} from 'react'
 import './styles.css'
 
 export default function BarraDePesquisa(){
 
     function InputPesquisa({placeholder, classe}){
+        const [termoPesquisa, setPesquisa] = useState('')
         return(
-            <input type="search" placeholder={placeholder} className={classe} />
+            <input type="search" placeholder={placeholder} className={classe} value={termoPesquisa} setValor={setPesquisa} />
         )
     }
     return(
