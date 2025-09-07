163ecfefe2f63300f8d1207aa286b0930c9b0c04
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2025-09-06 21:10:24 -0300

    feat: Componente Botão filtro

--- src/componentes/filtros/index.jsx
+++ src/componentes/filtros/index.jsx
@@ -1,1 +1,16 @@
 import './styles.css'
+
+export default function Filtros(){
+    function BotaoFiltro({children}){
+        return(
+            <button className='btn-filtro' >{children}</button>
+        )
+    }
+    return(
+        <div>
+            <BotaoFiltro>Front-end</BotaoFiltro>
+            <BotaoFiltro>React</BotaoFiltro>
+            <BotaoFiltro>Acessibilidade</BotaoFiltro>
+        </div>
+    )
+}
